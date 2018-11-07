376bf7971559b8bdaccec2cf2ba25053721ea7d7
rslgp <rslgp@cin.ufpe.br>
2018-11-07 15:14:17 -0300

    component navBarHeader + doing fix responsible

---
+++ findMySpace/src/components/shared/naviBarHeader.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+  <nav class="navbar" style="background-color: #73567c">
+    <div class="navbar-collapse collapse w-100 order-1 order-md-0 dual-collapse2">       
+		<a class="title"><img src="./assets/logo-white.png"></a>
+    </div>
+    <div class="navbar-collapse collapse order-3 dual-collapse2" style="display: none;" >
+        <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+                <a class="nav-link" href="#">
+					<naviBar/>
+				</a>
+            </li>
+        </ul>
+    </div>
+</nav>
+
+
+  </div>
+</template>
+
+<script>
+import naviBar from './naviBar'
+export default {
+  components: {
+	naviBar
+  }  
+}
+</script>
+
+<style scoped>
+</style>
