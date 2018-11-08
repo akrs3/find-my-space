37ff13ef3f1d4cdc206dcb6bc815a0d789d56ae4
rslgp <rslgp@cin.ufpe.br>
2018-11-07 21:49:55 -0300

    completed navibarheader

--- findMySpace/src/components/shared/naviBarHeader.vue
+++ findMySpace/src/components/shared/naviBarHeader.vue
@@ -1,22 +1,32 @@
 <template>
-  <div>
-  <nav class="navbar navbar-expand-md navbar-dark" style="background-color: #73567c">
-    <div class="navbar-collapse collapse w-100 order-1 order-md-0 dual-collapse2">       
-	<a class="title"><img src="./assets/logo-white.png"></a>
-    </div>
-    <div class="navbar-collapse collapse order-3 dual-collapse2" style="display: none;" >
-        <ul class="navbar-nav ml-auto">
-            <li class="nav-item">
-                <a class="nav-link" href="#">
-					<naviBar/>
-				</a>
-            </li>
-        </ul>
-    </div>
-</nav>
+	<div>
+  <!-- bootstap nao estava adaptivo (qnd dava resize na pagina nao ajustava os itens)
+	  <nav class="navbar navbar-expand-md navbar-dark" style="background-color: #73567c">
+			<div class="navbar-collapse collapse w-100 order-1 order-md-0 dual-collapse2">       
+			<img align="left" src="./assets/logo-white.png">
+			</div>
+			<div class="navbar-collapse collapse order-3 dual-collapse2" style="display: none;" >
+				<ul class="navbar-nav ml-auto">
+					<li class="nav-item">
+						<a class="nav-link" href="#">
+							<naviBar/>
+						</a>
+					</li>
+				</ul>
+			</div>
+		</nav>
+	-->
+	
+		<ul class="naviBarHeaderBar">
+			<li class="naviBarHeaderLogo">
+				<img src="./assets/logo-white.png">
+			</li>
+		  <li class="naviBarHeaderGridButton">
+			<naviBar/>
+		  </li>
+		</ul>
 
-
-  </div>
+	</div>
 </template>
 
 <script>
@@ -29,4 +39,36 @@ export default {
 </script>
 
 <style scoped>
+
+.navbar {
+
+/*bootstrap
+    -webkit-border-radius: 0;
+    -moz-border-radius: 0;
+    border-radius: 0;
+	padding: 0;
+	padding-left: 1rem;
+	*/
+}
+
+.naviBarHeaderBar {
+	background-color: #73567c;
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+	margin-bottom: 20px;
+    overflow: hidden;
+}
+
+.naviBarHeaderGridButton{
+	float:right; 
+	color:#c4b7c7;
+
+}
+
+.naviBarHeaderLogo{
+	float:left;
+}
+
+
 </style>
