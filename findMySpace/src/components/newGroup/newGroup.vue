03dbeeb2471e733f30bfc42d9497bea1e79db31f
Daniel Barbosa Maranhão <dbm2@cin.ufpe.br>
2018-11-09 13:40:15 -0300

    creating shared components of rounded button and back button; implementing space screen

--- findMySpace/src/components/newGroup/newGroup.vue
+++ findMySpace/src/components/newGroup/newGroup.vue
@@ -6,16 +6,13 @@
 
       	<div class ="new-group-content">
 	      	<!-- Back Button -->
-	      	<div style=" display: flex; align-items: center;" align="left">
-	      		<button class="backButton" v-on:click="$router.go(-1)" />
-	      		<span style="margin-left: 20px; font-size: 12pt">voltar</span>
-	      	</div>
+	      	<backButton />
 	      
 	      	<!-- Header -->
 	      	<div style="margin-top:10px" align="left">
 		      	<span style="font-weight: bold; font-size: 14pt;">cadastrar novo grupo</span>
 		      	<hr/>
-		    </div>
+		      </div>
 
 	      	<!-- Group Attributes -->
 	      	<div style="margin-top:30px" align="left">
@@ -29,24 +26,27 @@
 		      	<span style="color: #757376; font-size: 11pt">envie esse link para os membros se juntarem</span>
 		      	<br/>
 		      	<span style="font-weight: bold; font-size: 15pt; word-wrap: break-word">
-		      	{{ groupInviteURL }} 
+		      	 {{ groupInviteURL }} 
 		      	</span>
+		      </div>
 		    </div>
-		</div>
 
-	    <!-- Create Button -->
-	    <div>
-	    	<button class="createButton" v-on:click="createGroup">
-	    		concluído!
-	    	</button>
-	    </div>
+  	    <!-- Create Button -->
+        <roundedButton title='concluído!' compressed='false' v-bind:handler='createGroup' />
+
       </div>
+
   </div>
 </template>
 
 <script>
+import backButton from '../shared/backButton'
+import roundedButton from '../shared/roundedButton'
+
 export default {
   components: {
+    roundedButton,
+    backButton
   },
 
   data() {
@@ -58,7 +58,7 @@ export default {
 
   methods: { 
 
-  		createGroup: function(event) {
+  		createGroup: function() {
   			let groupName = this.groupName
 
   			alert(groupName)
@@ -80,7 +80,7 @@ export default {
 .new-group {
 	//font-family: 'Nunito-Regular';
 	min-height:100%;
-   	position:relative;
+  position:relative;
 }
 
 .new-group-content {
@@ -121,36 +121,6 @@ export default {
 :-moz-placeholder { /* Firefox 4 - 18 */
   color: #6E5077;
   opacity: 0.75;
-}
-
-.backButton {
-    background-image: url( '../../assets/back_button.png' );
-    background-size: 10px 20px;
-    background-repeat: no-repeat;
-    background-position: center; 
-    padding: 0px 0px;
-    border: none;
-    height: 44px;  
-    width: 10px;
-    color: #123123;
-}
-
-.backButton:focus {
-	outline:0 !important;
-}
-
-.createButton {
-	width: 100%;
-    background-color: #6E5077;
-    border: none;
-    color: white;
-    padding: 20px;
-    text-align: center;
-    text-decoration: none;
-    display: inline-block;
-    font-size: 15px;
-    cursor: pointer;
-    border-radius: 12px;
 }
 
 </style>
---
+++ findMySpace/src/components/shared/backButton.vue
@@ -0,0 +1,32 @@
+<template>
+  <div style=" display: flex; align-items: center;" align="left">
+      <button class="backButton" v-on:click="$router.go(-1)" />
+      <span style="margin-left: 20px; font-size: 12pt">voltar</span>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+
+.backButton {
+  background-image: url( '../../assets/back_button.png' );
+  background-size: 10px 20px;
+  background-repeat: no-repeat;
+  background-position: center; 
+  padding: 0px 0px;
+  border: none;
+  height: 44px;  
+  width: 10px;
+  color: #123123;
+}
+
+.backButton:focus {
+  outline:0 !important;
+}
+
+</style>
