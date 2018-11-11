fe90008cf4b48c242267bb7de6cba15068c6c302
rslgp <rslgp@cin.ufpe.br>
2018-11-11 19:22:28 -0300

    fix DEFINITIVO do bug geral do botao grid lateral

--- findMySpace/src/components/shared/naviBar.vue
+++ findMySpace/src/components/shared/naviBar.vue
@@ -26,7 +26,20 @@
 			
 			<!-- LOGIN -->
 			  <b-modal size="sm" align="left" id="myModal">
-				<login/>
+				<table align="center">
+					<tr>
+						<p class="queryCadSpac" align="left">Usuário: </p>
+						
+						
+								<input align="left" type="text" name="user">
+					</tr>
+					
+					<tr align="left">
+						<p class="queryCadSpac" align="left">Senha: </p>
+						
+								<input align="left" type="password" name="senha">
+					</tr>
+				</table>
 			  </b-modal>
 	
   </div>
@@ -34,10 +47,9 @@
 
 <script>
 import designUX from '../../assets/css/designUX.css'
-import login from '../login/login'
+
 export default {
   components: {
-	login  
   } 
 }
 </script>
@@ -122,5 +134,30 @@ hr{
   .sidenav {padding-top: 15px;}
   .sidenav a {font-size: 18px;}
   .menuNav {margin-top:40px}
+}
+
+
+input {
+	float: left;
+	background: transparent;
+	border: none;
+	font-weight: bold;
+	font-size: 20px;
+	
+	
+    border: 0px solid red;
+    padding-left: 20px;
+	margin-bottom: 20px;
+    border-radius: 25px;
+    background-color: #bbadbf;
+	opacity: 0.5;
+	
+	width: 100%; height: 44px !important;
+}
+
+.queryCadSpac {
+	color:#6e5077;
+	margin-left: 6%;
+	margin-right: 6%;
 }
 </style>
