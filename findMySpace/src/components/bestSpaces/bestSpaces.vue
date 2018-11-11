68785d2fbda993185c92613f3c15a6037646e48d
rslgp <rslgp@cin.ufpe.br>
2018-11-11 16:48:50 -0300

    hotfix merge assets folder + fix import deles + fix navBar bestspaces

--- findMySpace/src/components/bestSpaces/bestSpaces.vue
+++ findMySpace/src/components/bestSpaces/bestSpaces.vue
@@ -1,5 +1,7 @@
 <template>
     <div>
+		
+		<naviBarHeader/>
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"> <!-- cdn não é a melhor forma, mas eu nao lembro como faz com webpack -->
         
         <div class="container">
