954f23988d3feb0488e64daa10f6dbf162fb558f
Eduardo Leite <elsn2@cin.ufpe.br>
2018-12-02 14:37:45 -0300

    backend quase pronto

--- findMySpace/src/components/space/cadastrarSpace2.vue
+++ findMySpace/src/components/space/cadastrarSpace2.vue
@@ -16,17 +16,17 @@
         <table align="left">
           <tr>
             <p class="queryCadSpac" align="left">qual o nome do seu espaço?</p>
-            <input align="left" type="text" name="nomeSpace" placeholder="Awesome Space">
+            <input align="left" type="text" name="nomeSpace" v-model="name" placeholder="Awesome Space">
           </tr>
           
           <tr>
             <p class="queryCadSpac" align="left">delimite o custo por hora <strong>R$ /h</strong></p>
-            <input style="width:100px" align="left" type="number" name="custoHora" placeholder="50">
+            <input style="width:100px" align="left" type="number" v-model="pricePerHour" name="custoHora" placeholder="50">
           </tr>
           
           <tr>
             <p class="queryCadSpac" align="left">endereço do espaço</p>
-            <input align="left" type="text" name="endSpace" placeholder="Rua do melhor, n 10">
+            <input align="left" type="text" name="endSpace" v-model="neighborhood" placeholder="Rua do melhor, n 10">
           </tr>
           
           <tr>
@@ -37,8 +37,10 @@
           <tr>
             <p class="queryCadSpac" align="left">adicione algumas fotos do seu espaço</p>
             
-            <div class="fotoArea"> 
-              <button onclick="alert('ok!')" class="centerImg"/>
+            <div class="fotoArea">
+              <label for="files" class="btn centerImg"></label>
+              <input id="files" class="hiddenInput" style="visibility:hidden;" type="file" @change="onFileChanged"> 
+              <!-- <button onclick="alert('ok!')" class="centerImg"/> -->
             </div>
           </tr>
           
@@ -48,9 +50,7 @@
         </table>
       </form>
     </div>
-    <router-link to="espaco">
-      <roundedButton title="cadastrar"></roundedButton>
-    </router-link>
+      <roundedButton :handler="onUpload" title="cadastrar"></roundedButton>
   </div>
 </template>
 
@@ -65,7 +65,55 @@
       naviBarHeader,
       roundedButton,
       backButton
-    }  
+    },
+    data() {
+      return {
+        selectedFile: null,
+        photo: '',
+        error: '',
+        name: '',
+        pricePerHour: '',
+        neighborhood: '',
+        rating: 0
+
+      }
+    },
+    methods: {
+      onFileChanged (event) {
+        this.selectedFile = event.target.files[0]
+        console.log(this.selectedFile)
+      },
+      
+      generateBase64: function generateBase64() {
+      var _this = this;
+      var canvas = document.createElement('CANVAS');
+      var img = document.createElement('img');
+      this.url = URL.createObjectURL(this.selectedFile);
+      img.src = this.url;
+      console.log("teste3")
+      console.log(img)
+      img.onload = function () {
+        canvas.height = img.height;
+        canvas.width = img.width;
+        console.log("teste2")
+        _this.photo = canvas.toDataURL('image/png');
+        console.log(_this.photo)
+        canvas = null;
+      };
+      img.onerror = function (error) {
+        _this.error = 'Could not load image, please check that the file is accessible and an image!';
+      };
+    },
+    onUpload() {
+        console.log("teste")
+        this.generateBase64()
+        console.log(this.base64)
+        console.log(this.name)
+        console.log(this.neighborhood)
+        console.log(this.pricePerHour)
+      // upload file, get it from this.selectedFile
+      },   
+    }
   }
 </script>
 
@@ -122,6 +170,9 @@
     border: none;
     background: url("../../assets/ownerProfile/addProfile.png") no-repeat center center;
     width: 100%; height: 100%; min-height: 180px;
+  }
+  .hiddenInput {
+    width: 100%; height: 0;
   }
 
   .bodyCadSpac {
