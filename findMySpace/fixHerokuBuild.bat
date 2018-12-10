d189205e059f4ddb996688b762c3ab1e58cfcde4
rslgp <rslgp@cin.ufpe.br>
2018-12-10 07:48:30 -0200

    use current index.html + copy firebase to dist

--- findMySpace/fixHerokuBuild.bat
+++ findMySpace/fixHerokuBuild.bat
@@ -2,4 +2,6 @@ mkdir .\dist\dist
 
 robocopy .\dist\ .\dist\dist /xf "dist" "index.html" /mov
 
-MOVE .\index.html .\dist
+copy .\src\firebase\firebaseManager.js .\dist\dist
+
+REM MOVE .\index.html .\dist
